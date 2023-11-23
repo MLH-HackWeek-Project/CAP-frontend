615e1f948a5c9053002be4181620c1b56a0b53e4
deluxesande <deluxesande@gmail.com>
2023-11-23 07:56:55 +0300

    designed the aside for user info display

--- src/components/utils/Aside.jsx
+++ src/components/utils/Aside.jsx
@@ -1,7 +1,38 @@
 import "../css/Aside.css";
 
 const Aside = () => {
-    return <div className="home-aside">Aside</div>;
+    const activities = ["Signed up", "Applied for a job"];
+
+    return (
+        <div className="home-aside">
+            <div className="user-info">
+                <div className="greet">
+                    <img
+                        className="user-avatar"
+                        src="src/assets/user.jpg"
+                        alt="User avatar"
+                    />
+                    <p>
+                        Hello,
+                        <br />
+                        <span className="username">John</span>
+                    </p>
+                </div>
+            </div>
+            <hr />
+            <div className="recents">
+                <h3>Recent Activity</h3>
+                <div className="activity-list">
+                    {/* <span className="no-content">No Content</span> */}
+                    {activities.map((activity, index) => (
+                        <div key={index} className="activity">
+                            {activity}
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Aside;
